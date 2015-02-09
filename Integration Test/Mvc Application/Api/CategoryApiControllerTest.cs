b3d9cbd3f7bae480e5306a7d5325bd7f5ecf0e4b
Alessandro Piccione <alex.75@tin.it>
2015-02-09 23:38:01 +0100

    Added missing file

---
+++ Integration Test/Mvc Application/Api/CategoryApiControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using FakeItEasy;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.Hosting.Builder;
+using Monei.DataAccessLayer.Interfaces;
+using Monei.Entities;
+using Monei.MvcApplication.Api;
+using NUnit.Framework;
+using Owin;
+using Should;
+
+
+namespace Monei.Test.IntegrationTest.MvcApplication.Api
+{
+	[TestFixture, Category("API"), Category("Category")]
+	public class CategorApiyControllerTest :ApiControllerTestBase
+	{
+		private readonly string routePrefix = "api/category/";
+
+		[Test]
+		public void Ping_Should_RespondOk()
+		{
+			using (var client = GetClient())
+			using (var request = CreateRequest(routePrefix + "ping", HttpMethod.Get))
+			using (var response = client.SendAsync(request).Result)
+			{
+				response.IsSuccessStatusCode.ShouldBeTrue();
+			}
+			
+		}
+
+		[Test]
+		public void List_Should_ReturnAList()
+		{
+			string url = routePrefix + "list";
+			IEnumerable<Category> returnedList;
+
+
+			returnedList = CallApi<IEnumerable<Category>>(url, HttpMethod.Get);
+							
+			// Verify
+			returnedList.ShouldNotBeNull();
+			returnedList.ShouldNotBeEmpty();
+		}
+
+		
+		
+	}
+}
