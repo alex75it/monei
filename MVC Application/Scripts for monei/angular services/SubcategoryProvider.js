9031d735c93e9528b958d25a99bba5d93af04cb0
Alessandro Piccione <alex.75@tin.it>
2015-02-18 22:10:57 +0100

    Created SubcategoryProvider Angular service

---
+++ MVC Application/Scripts for monei/angular services/SubcategoryProvider.js
@@ -0,0 +1,62 @@
+﻿app.service("subcategoryProvider",
+["$http",
+function($http) {
+
+	var baseUrl = "/api/subcategory/";
+
+	var service = {};
+
+	service.getSubcategories = function(callback, errorCallback, finallyCallback) {
+		$http.get(baseUrl)
+			.success(function(data, status) { callback(data) })
+			.error(function(error, status) {errorCallback && errorCallback(error)})
+			.finally(function() { finallyCallback && finallyCallback() })
+		;
+	};
+
+	return service;
+
+}]);
+
+
+/// Jasmine tests
+describe("subcategoryProvider", function () {
+
+	//var subcategoryProvider;
+	var appName = app.name; // this "initialize" module, all other way give an error
+	var $httpBackend;
+	var requestHandler;
+
+	beforeEach(angular.mock.module("monei"));
+	beforeEach(inject(function($injector) {
+		$httpBackend = $injector.get("$httpBackend");
+		requestHandler = $httpBackend.when("GET", "/api/subcategory/").respond([]);
+	}));
+
+	it("contains getSubcategories function", function () {
+		inject(function (subcategoryProvider) {
+			expect(subcategoryProvider.getSubcategories).toBeDefined();
+		});
+	});
+
+	it("when getSubcategories is called it call success callback", function() {
+		inject(function (subcategoryProvider) {
+			requestHandler.respond([{ name: "subcategory A" }]);
+			$httpBackend.expectGET("/api/subcategory/");
+
+			var result;
+			var success = function(data) { result = data; } ;
+			//spyOn(a, "success");  // call to success
+			subcategoryProvider.getSubcategories(success);
+			
+			$httpBackend.flush();
+
+			//expect(success).toHaveBeenCalled();
+			expect(result).not.toBeUndefined();
+			expect(result).not.toBeNull();
+			expect(result.length).toBe(1);
+			expect(result[0].name).toEqual("subcategory A");
+		});
+	});
+
+});
