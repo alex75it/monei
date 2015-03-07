dac92378b7132d222af3f9bbcc06775013afeba3
Alessandro Piccione <alex.75@tin.it>
2015-03-07 21:22:51 +0100

    Renamed "registryProvider" to "registryDataProvider". Added tests for RegistryDataProvider.

---
+++ MVC Application/Scripts for monei/angular providers/RegistryDataProvider.js
@@ -0,0 +1,15 @@
+﻿app.factory("registryDataProvider",
+["$http",
+function ($http) {
+	var baseUrl = "/api/registry/";
+	var provider = {};
+
+	provider.search = function(filters, callback, errorCallback, finallyCallback) {
+		$http.post(baseUrl + "search", filters)
+			.success(callback)
+			.error(function(error, status) { errorCallback && errorCallback(error) })
+			.finally(function() { finallyCallback && finallyCallback() });
+	};
+	
+	return provider;
+}]);
---
+++ MVC Application/Scripts for monei/angular providers/RegistryDataProvider_test.js
@@ -0,0 +1,69 @@
+﻿"use strict";
+/// Jasmine tests
+describe("registryDataProvider", function () {
+
+	var homeCategoryId = 3;
+
+	var appName = app.name; // this "initialize" module, all other way give an error
+	beforeEach(angular.mock.module("monei")); // this is needed to make injection of providers works
+
+	var baseUrl = "/api/registry/";
+
+	// mock the Angular $http service
+	var requestHandler;
+	var httpBackend;
+	beforeEach(inject(function ($injector) {
+		httpBackend = $injector.get("$httpBackend");
+	}));
+
+	it("contains \"search\" function", function () {
+		inject(function (registryDataProvider) {
+			expect(registryDataProvider.search).toBeDefined();
+		});
+	});
+
+	it("when call \"search\" function the \"callback\" function is called", function () {
+		inject(function (registryDataProvider) {
+			// Arrange
+			var url = baseUrl + "search";
+			httpBackend.expectPOST(url).respond([{ id: 123, name: "Home" }]);
+
+			var callbackSpy = {
+				success: function (data) { }
+			};
+			spyOn(callbackSpy, "success");
+
+			// Act
+			var postData = {};
+			registryDataProvider.search(postData, callbackSpy.success);
+			httpBackend.flush();
+
+			// Assert
+			expect(callbackSpy.success).toHaveBeenCalled();
+		});
+	});
+
+	it("when call \"search\" returns data", function () {
+		inject(function (registryDataProvider) {
+			// Arrange
+			var url = baseUrl + "search";
+			httpBackend.expectPOST(url).respond([{ id: 123, date: "2015-03-08" }]);
+
+			var returnData = "null";
+			var success = function (data) { returnData = data; };
+
+			// Act
+			var postData = { categories: [homeCategoryId] };
+			registryDataProvider.search(postData, success);
+			httpBackend.flush();
+
+			// Assert
+			expect(returnData).not.toBeUndefined();
+			expect(returnData).not.toBeNull();
+			expect(returnData.length).toBe(1);
+			expect(returnData[0].date).toEqual("2015-03-08");
+		});
+	});
+
+
+});
