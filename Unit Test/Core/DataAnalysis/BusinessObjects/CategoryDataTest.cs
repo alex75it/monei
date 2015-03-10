96c713bdec4af257d6ce34a8cd3187cb6285734e
Alessandro Piccione <alex.75@tin.it>
2015-03-10 23:00:04 +0100

    Added tests for BusinessDataObjects in DataAnalysis

---
+++ Unit Test/Core/DataAnalysis/BusinessObjects/CategoryDataTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monei.Core.DataAnalysis.BusinessObjects;
+using Monei.Entities;
+using NUnit.Framework;
+using Should;
+
+namespace Monei.Test.UnitTest.Core.DataAnalysis.BusinessObjects
+{
+	[TestFixture]
+	public class CategoryDataTest
+	{
+		[Test]
+		public void Ctor()
+		{
+			Category category = new Category();
+			category.Name = "Cat A";
+			decimal amount = 123m;
+
+			var data = new CategoryData(category, amount);
+
+			data.Amount.ShouldEqual(amount);
+			data.Name.ShouldEqual(category.Name);
+			data.Id.ShouldEqual(0);
+		}
+	}
+}
