d8c27b08a3e550b641e8a2fee32cad9d24a208ee
Alessandro Piccione <alex.75@tin.it>
2015-03-11 19:41:35 +0100

    Removed "NavigationRoutes" (is it added by a Bootstrap MVC Example NuGet packages?). Removed unexistent path from BundleConfig.

--- MVC Application/App_Start/BundleConfig.cs
+++ MVC Application/App_Start/BundleConfig.cs
@@ -72,7 +72,6 @@ namespace Monei.MvcApplication
 				"~/Scripts for monei/angular app.js",
 				"~/Scripts for monei/angular controllers/*.js",
 				"~/Scripts for monei/angular directives/*.js",
-				"~/Scripts for monei/angular services/*.js",
 				"~/Scripts for monei/angular providers/*.js",
 				"~/Scripts for monei/angular filters/*.js"
 				)
