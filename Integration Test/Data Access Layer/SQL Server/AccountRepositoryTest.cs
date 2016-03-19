977e885e06a5b03e52441e9549acc9baf1a8f2fd
Alessandro Piccione <alex.75@tin.it>
2016-03-20 00:53:33 +0100

    Correct test

--- Integration Test/Data Access Layer/SQL Server/AccountRepositoryTest.cs
+++ Integration Test/Data Access Layer/SQL Server/AccountRepositoryTest.cs
@@ -13,184 +13,184 @@ using Should;
 
 namespace Monei.Test.IntegrationTest.DataAccessLayer.SqlServer
 {
-	[TestFixture]
-	public class AccountRepositoryTest : RepositoryTestBase
-	{
+    [TestFixture]
+    public class AccountRepositoryTest : RepositoryTestBase
+    {
 
 
-		[TestFixtureSetUp]
-		public void ClassInitialize()
-		{
-			//Helper.RemoveTestAccount();
-		}
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            //Helper.RemoveTestAccount();
+        }
 
-		[SetUp]
-		public void TestInizialize()
-		{
-			try
-			{
-				Helper.RemoveTestAccount();
-			}
-			catch (Exception exc)
-			{
-				Assert.Inconclusive("FAIL to delete Account test. " + exc.Message);
-			}
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            try
+            {
+                Helper.RemoveTestAccount();
+            }
+            catch (Exception exc)
+            {
+                Assert.Inconclusive("FAIL to delete Account test. " + exc.Message);
+            }
+        }
 
-		[TearDown]
-		public void TestCleanup()
-		{
-			Helper.RemoveTestAccount();
-		}
-
-
-
-		[Test]
-		public void CreateUserAndAccount()
-		{
-			//todo: 6s for run this test?
-			//todo: this test fail (the creation date day is wrong) when executed between 24:00 and 01:00
-
-			string username = Helper.TEST_USERNAME;
-			string password = "password_test";
-			Account.AccountRole role = Account.AccountRole.User;
-			Currency currency = Helper.GetEuroCurrency();
-
-			Account account = AccountRepository.Create(username, password, role, currency);
-
-			Assert.IsTrue(true);
-			Assert.AreEqual(username, account.Username, "Username");
-			Assert.AreEqual(password, account.Password);
-			Assert.AreEqual(role, account.Role);
-			Assert.AreEqual(currency.Id, account.Currency.Id);
-			Assert.AreEqual(DateTime.Today, account.CreationDate.Date, "CreationDate");
-
-		}
+        [TearDown]
+        public void TestCleanup()
+        {
+            Helper.RemoveTestAccount();
+        }
 
 
-		[Test]
-		public void Create()
-		{
-			string username = "TEST";
-			string password = "test";
-			Account.AccountRole role = Account.AccountRole.User ;
-			Currency currency = Helper.GetEuroCurrency();
 
-			Account account = Account.Create(username, password, role, currency); 
-				
+        [Test]
+        public void CreateUserAndAccount()
+        {
+            //todo: 6s for run this test?
+            //todo: this test fail (the creation date day is wrong) when executed between 24:00 and 01:00
 
-			account = AccountRepository.Create(account);
+            string username = Helper.TEST_USERNAME;
+            string password = "password_test";
+            Account.AccountRole role = Account.AccountRole.User;
+            Currency currency = Helper.GetEuroCurrency();
 
+            Account account = AccountRepository.Create(username, password, role, currency);
 
-			Account loadAccount = AccountRepository.Read(account.Username);
+            Assert.IsTrue(true);
+            Assert.AreEqual(username, account.Username, "Username");
+            Assert.AreEqual(password, account.Password);
+            Assert.AreEqual(role, account.Role);
+            Assert.AreEqual(currency.Id, account.Currency.Id);
+            Assert.AreEqual(DateTime.Today, account.CreationDate.Date, "CreationDate");
 
-			Assert.AreEqual(username, loadAccount.Username);
-
-			// username exists
-
-			Account account_2 = Account.Create(username, password, role, currency);
-						
-
-			//Action action = a => AccountRepository.Create(a);
-			//Action<Account> action = a => AccountRepository.Create(a);
-			//Should.ActionAssertionExtensions
-			//action.ShouldThrow();
-
-			try
-			{
-				AccountRepository.Create(account_2);
-				Assert.Fail("Duplicate username haven't tot be permitted.");
-			}
-			catch(EntityAlreadyExistsException)
-			{
-				// expected
-			}
+        }
 
 
-			// clean 
-			AccountRepository.Delete(account.Id);
-		}
+        /// Todo: split this test in more tests
+        [Test]
+        public void Create()
+        {
+            string username = Helper.TEST_USERNAME;
+            string password = "test";
+            Account.AccountRole role = Account.AccountRole.User;
+            Currency currency = Helper.GetEuroCurrency();
+
+            Account account = Account.Create(username, password, role, currency);
+
+            account = AccountRepository.Create(account);
 
 
-		[Test]
-		public void Read()
-		{
-			string username = "Test";
-			string password = "test";
-			Account.AccountRole role = Account.AccountRole.User;
-			Currency currency = Helper.GetEuroCurrency();
+            Account loadAccount = AccountRepository.Read(account.Username);
 
-			Account account = Account.Create(username, password, role, currency);
+            Assert.AreEqual(username, loadAccount.Username);
 
-			account = AccountRepository.Create(account);
+            // username exists
 
-			Account loadedAccount = AccountRepository.Read(account.Id);
-			loadedAccount = AccountRepository.Read(account.Username);
-
-			Assert.AreEqual(username, loadedAccount.Username);
-			Assert.AreEqual(password, loadedAccount.Password);
-			Assert.AreEqual(role, loadedAccount.Role);
-			Assert.AreEqual(currency.Id, loadedAccount.Currency.Id);
-			
-			// clean 
-			AccountRepository.Delete(account.Id);
-		}
-
-		[Test]
-		public void Update()
-		{
-
-			string username = "Test";
-			string password = "test";
-			Account.AccountRole role = Account.AccountRole.User;
-			Currency currency = Helper.GetEuroCurrency();
-
-			Account account = Account.Create(username, password, role, currency);
-
-			// create
-			account = AccountRepository.Create(account);
-
-			// change some properties
-			string newPassword = "aaaa123";
-			Account.AccountRole newRole =  Account.AccountRole.Administrator;
-			//Currency			
-			account.Password = newPassword;
-			account.Role = newRole;
-
-			// update
-			account = AccountRepository.Update(account);
-
-			// test
-			account = AccountRepository.Read(account.Id);
-
-			Assert.AreEqual(newPassword, account.Password);
-			Assert.AreEqual(newRole, account.Role);
+            Account account_2 = Account.Create(username, password, role, currency);
 
 
-			// clean 
-			AccountRepository.Delete(account.Id);
-		}
+            //Action action = a => AccountRepository.Create(a);
+            //Action<Account> action = a => AccountRepository.Create(a);
+            //Should.ActionAssertionExtensions
+            //action.ShouldThrow();
+
+            try
+            {
+                AccountRepository.Create(account_2);
+                Assert.Fail("Duplicate username haven't tot be permitted.");
+            }
+            catch (EntityAlreadyExistsException)
+            {
+                // expected
+            }
 
 
-		[Test]
-		public void SetLastLoginDate()
-		{
-			Account account = Helper.GetTestAccount();
+            // clean 
+            AccountRepository.Delete(account.Id);
+        }
 
-			int accountId = account.Id;
-			DateTime lastLogin = DateTime.Now.AddHours(-2);
-			AccountRepository.UpdateLastLogin(accountId, lastLogin);
 
-		}
+        [Test]
+        public void Read()
+        {
+            string username = "Test";
+            string password = "test";
+            Account.AccountRole role = Account.AccountRole.User;
+            Currency currency = Helper.GetEuroCurrency();
 
-		[Test]
-		public void GelAllAccounts()
-		{
-			IList<Account> accounts = AccountRepository.ListAll();
+            Account account = Account.Create(username, password, role, currency);
 
-			Assert.IsTrue(accounts.Count > 0);
-			Assert.IsTrue(accounts.Where(a => a.Username.ToUpper() == "ALEX").Single() != null );
-		}
+            account = AccountRepository.Create(account);
 
-	}
+            Account loadedAccount = AccountRepository.Read(account.Id);
+            loadedAccount = AccountRepository.Read(account.Username);
+
+            Assert.AreEqual(username, loadedAccount.Username);
+            Assert.AreEqual(password, loadedAccount.Password);
+            Assert.AreEqual(role, loadedAccount.Role);
+            Assert.AreEqual(currency.Id, loadedAccount.Currency.Id);
+
+            // clean 
+            AccountRepository.Delete(account.Id);
+        }
+
+        [Test]
+        public void Update()
+        {
+
+            string username = "Test";
+            string password = "test";
+            Account.AccountRole role = Account.AccountRole.User;
+            Currency currency = Helper.GetEuroCurrency();
+
+            Account account = Account.Create(username, password, role, currency);
+
+            // create
+            account = AccountRepository.Create(account);
+
+            // change some properties
+            string newPassword = "aaaa123";
+            Account.AccountRole newRole = Account.AccountRole.Administrator;
+            //Currency			
+            account.Password = newPassword;
+            account.Role = newRole;
+
+            // update
+            account = AccountRepository.Update(account);
+
+            // test
+            account = AccountRepository.Read(account.Id);
+
+            Assert.AreEqual(newPassword, account.Password);
+            Assert.AreEqual(newRole, account.Role);
+
+
+            // clean 
+            AccountRepository.Delete(account.Id);
+        }
+
+
+        [Test]
+        public void SetLastLoginDate()
+        {
+            Account account = Helper.GetTestAccount();
+
+            int accountId = account.Id;
+            DateTime lastLogin = DateTime.Now.AddHours(-2);
+            AccountRepository.UpdateLastLogin(accountId, lastLogin);
+
+        }
+
+        [Test]
+        public void GelAllAccounts()
+        {
+            IList<Account> accounts = AccountRepository.ListAll();
+
+            Assert.IsTrue(accounts.Count > 0);
+            Assert.IsTrue(accounts.Where(a => a.Username.ToUpper() == "ALEX").Single() != null);
+        }
+
+    }
 }
