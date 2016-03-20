cbc05cfe00fd079aa24ac0635fcfdb1cca38ee68
Alessandro Piccione <alex.75@tin.it>
2016-03-20 17:20:47 +0100

    Correct connection string.

--- Data Access Layer/SQL Server/RepositoryBase.cs
+++ Data Access Layer/SQL Server/RepositoryBase.cs
@@ -9,77 +9,92 @@ using NHibernate;
 
 namespace Monei.DataAccessLayer.SqlServer
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <typeparam name="TEntity">Entity managed by this repository</typeparam>
-	public class RepositoryBase<TKey, TEntity>: IRepository<TKey, TEntity> //where TEntity: BaseEntity
-	{
-		protected string ConnectionString = ConfigurationManager.ConnectionStrings["monei"].ConnectionString;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity">Entity managed by this repository</typeparam>
+    public class RepositoryBase<TKey, TEntity>: IRepository<TKey, TEntity> //where TEntity: BaseEntity
+    {
+        private string connectionString;
+        protected string ConnectionString { get
+            {
+                if (connectionString == null)
+                {
+                    try
+                    {
+                        connectionString = ConfigurationManager.ConnectionStrings["monei"].ConnectionString;
+                    }
+                    catch (Exception exc)
+                    {
+                        throw new Exception("Fail to load connection string", exc);
+                    }
+                }
+                return ConnectionString;
+            }
+         }
+                
+        //Nhibernate session factory
+        //Component.For<ISessionFactory>().UsingFactoryMethod(CreateNhSessionFactory).LifeStyle.Singleton,
+
+        protected ISessionFactory GetSessionFactory()
+        { 
+            var configuration = new NHibernate.Cfg.Configuration();
+            //configuration.AddAssembly("Monei.Entities");
+            configuration.Configure();
+            
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            //NHibernate.Dialect.MsSql2012Dialect
+    
+            return sessionFactory;
+        }
+
+        protected ISession OpenSession()
+        {
+            return GetSessionFactory().OpenSession();
+            //return GetSessionFactory().GetCurrentSession;
+        }
+
+        protected IStatelessSession OpenStatelessSession()
+        {
+            return GetSessionFactory().OpenStatelessSession();
+        }
 
 
-		//Nhibernate session factory
-		//Component.For<ISessionFactory>().UsingFactoryMethod(CreateNhSessionFactory).LifeStyle.Singleton,
+        public TKey Create(TEntity data)
+        {
+            using (ISession session = OpenSession())
+            {
+                return (TKey)session.Save(data);
+            }
+        }
 
-		protected ISessionFactory GetSessionFactory()
-		{ 
-			var configuration = new NHibernate.Cfg.Configuration();
-			//configuration.AddAssembly("Monei.Entities");
-			configuration.Configure();
-			
-			ISessionFactory sessionFactory = configuration.BuildSessionFactory();
-			//NHibernate.Dialect.MsSql2012Dialect
-	
-			return sessionFactory;
-		}
+        public TEntity Read(TKey key)
+        {
+            using (ISession session = OpenSession())
+            {
+                return session.Get<TEntity>(key);
+            }
+        }
 
-		protected ISession OpenSession()
-		{
-			return GetSessionFactory().OpenSession();
-			//return GetSessionFactory().GetCurrentSession;
-		}
+        public void Update(TEntity data)
+        {
+            using (ISession session = OpenSession())
+            {
+                session.Update(data);
+                session.Flush();
+            }
+        }
 
-		protected IStatelessSession OpenStatelessSession()
-		{
-			return GetSessionFactory().OpenStatelessSession();
-		}
+        public void Delete(TKey key)
+        {
+            using (ISession session = OpenSession())
+            {
+                TEntity data = Read(key);
+                if (data != null)
+                    session.Delete(data);
+                session.Flush();
+            }
+        }
 
-
-		public TKey Create(TEntity data)
-		{
-			using (ISession session = OpenSession())
-			{
-				return (TKey)session.Save(data);
-			}
-		}
-
-		public TEntity Read(TKey key)
-		{
-			using (ISession session = OpenSession())
-			{
-				return session.Get<TEntity>(key);
-			}
-		}
-
-		public void Update(TEntity data)
-		{
-			using (ISession session = OpenSession())
-			{
-				session.Update(data);
-				session.Flush();
-			}
-		}
-
-		public void Delete(TKey key)
-		{
-			using (ISession session = OpenSession())
-			{
-				TEntity data = Read(key);
-				if (data != null)
-					session.Delete(data);
-				session.Flush();
-			}
-		}
-
-	}
+    }
 }
