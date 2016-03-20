d7cf27824dfcec54a4b22b1f0392db39aaee1b1d
Alessandro Piccione <alex.75@tin.it>
2016-03-20 17:20:59 +0100

    Correct test

--- Unit Test/Core/Managers/CategoryManagerTest.cs
+++ Unit Test/Core/Managers/CategoryManagerTest.cs
@@ -13,47 +13,48 @@ using Should;
 
 namespace Monei.Test.UnitTest.Core.Managers
 {
-	[TestFixture]
-	public class CategoryManagerTest
-	{
-		[Test]
-		public void Update_WhenCategoryHasATooLongName_Should_ThrowsSpecificException()
-		{
-			ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
-			CategoryManager manager = new CategoryManager(categoryRepository);
-			Category category = new Category()
-			{
-				Name = new String('a', 1000)
-			};
+    [TestFixture]
+    public class CategoryManagerTest
+    {
+        [Test]
+        public void Update_WhenCategoryHasATooLongName_Should_ThrowsSpecificException()
+        {
+            ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
+            CategoryManager manager = new CategoryManager(categoryRepository);
+            Category category = new Category()
+            {
+                Name = new String('a', 1000)
+            };
 
-			Assert.Throws<CategoryTooLongNameException>(() => manager.Update(category));
-		}
+            Assert.Throws<CategoryTooLongNameException>(() => manager.Update(category));
+        }
 
 
-		[Test]
-		public void Update_WhenCategoryHasATooShortName_Should_ThrowsException()
-		{
-			ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
-			CategoryManager manager = new CategoryManager(categoryRepository);
-			Category category = new Category()
-			{
-				Name = new String('a', Category.NAME_MIN_LENGTH-1)
-			};
+        [Test]
+        public void Update_WhenCategoryHasATooShortName_Should_ThrowsException()
+        {
+            ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
+            CategoryManager manager = new CategoryManager(categoryRepository);
+            Category category = new Category()
+            {
+                Name = new String('a', Category.NAME_MIN_LENGTH-1)
+            };
 
-			Assert.Throws<Exception>(() => manager.Update(category));
-		}
+            Assert.Throws<Exception>(() => manager.Update(category));
+        }
 
-		[Test]
-		public void Update_WhenCategoryHasATooLongDescription_Should_ThrowsException()
-		{
-			ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
-			CategoryManager manager = new CategoryManager(categoryRepository);
-			Category category = new Category()
-			{
-				Description = new String('a', Category.DESCRIPTION_MAX_LENGTH+1)
-			};
+        [Test]
+        public void Update_WhenCategoryHasATooLongDescription_Should_ThrowsException()
+        {
+            ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
+            CategoryManager manager = new CategoryManager(categoryRepository);
+            Category category = new Category()
+            {
+                Name = "aaa",
+                Description = new String('a', Category.DESCRIPTION_MAX_LENGTH+1)
+            };
 
-			Assert.Throws<Exception>(() => manager.Update(category));
-		}
-	}
+            Assert.Throws<Exception>(() => manager.Update(category));
+        }
+    }
 }
