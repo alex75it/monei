a5c0c7df177abc64957f38e57d8add1ea4d18c2d
Alex75 <alessandro.piccione.75@gmail.com>
2016-05-21 10:09:28 +0100

    Add check on NHibernate class mapping test.

--- Integration Test/Entities/NHibernate Mappings/NHibernateMappingsTest.cs
+++ Integration Test/Entities/NHibernate Mappings/NHibernateMappingsTest.cs
@@ -6,28 +6,32 @@ using NUnit.Framework;
 
 namespace Monei.Test.IntegrationTest.Entities.NHibernateMappings
 {
-	[TestFixture, Category("NHibernate")]
-	public class NHibernateMappingsTest
-	{
-		[Test]
-		public void AccountMappingTest()
-		{
+    [TestFixture, Category("NHibernate")]
+    public class NHibernateMappingsTest
+    {
+        [Test]
+        public void AccountMappingTest()
+        {
 
-			Assembly assembly = Assembly.GetAssembly(typeof(Monei.Entities.Account));
+            Assembly assembly = Assembly.GetAssembly(typeof(Monei.Entities.Account));
 
-			//string a = NHibernate.Cfg.Configuration.DefaultHibernateCfgFileName ;
+            //string a = NHibernate.Cfg.Configuration.DefaultHibernateCfgFileName ;
 
-			NHibernate.Cfg.Configuration configuration = new NHibernate.Cfg.Configuration();
-			configuration.Configure();
-			foreach(var mapping in configuration.ClassMappings)
-			{
-				Type type = mapping.MappedClass;
-			}
+            NHibernate.Cfg.Configuration configuration = new NHibernate.Cfg.Configuration();
+            //configuration.AddAssembly("Monei.Entities");
+            //configuration.Configure();
 
-			SchemaExport export = new SchemaExport(configuration);
-			export.SetOutputFile("NHibernate mappings.sql");
-			//export.Create()
-			export.Execute(true, false, false);
-		}
-	}
+            Assert.IsNotEmpty(configuration.ClassMappings, "None class mapping loaded");
+
+            foreach(var mapping in configuration.ClassMappings)
+            {
+                Type type = mapping.MappedClass;
+            }
+
+            SchemaExport export = new SchemaExport(configuration);
+            export.SetOutputFile("NHibernate mappings.sql");
+            //export.Create()
+            export.Execute(true, false, false);
+        }
+    }
 }
