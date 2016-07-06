d8fd6e71aafb04e29766cdedc68a8afc7cafb872
Alex75 <alessandro.piccione.75@gmail.com>
2016-07-06 22:18:05 +0100

    Directive exposes event on categories selection change.

--- MVC Application/Scripts for monei/angular directives/CategorySelector.js
+++ MVC Application/Scripts for monei/angular directives/CategorySelector.js
@@ -2,51 +2,61 @@
 ["utils", "CategoryDataProvider",
 function (utils, CategoryDataProvider) {
 
-	var directive = {};
-	directive.restrict = "E";
-	directive.replace = true;
-	directive.templateUrl = "/Scripts for monei/directive-templates/CategorySelector.html";
+    var directive = {};
+    directive.restrict = "E";
+    directive.replace = true;
+    directive.templateUrl = "/Scripts for monei/directive-templates/CategorySelector.html";
 
-	directive.link = function(scope, element, attrs) {
-		scope.loading = true;
+    directive.scope = {
+        selectedCategories: "=categories"
+    };
 
-		element.find("select").multiselect({
-			templates: {
-			    button: '<button type="button" class="multiselect dropdown-toggle form-control" data-toggle="dropdown"></button>'
-			},
-			buttonClass: 'btn btn-default',
-			buttonWidth: 'auto',
-			buttonContainer: '<div class="btn-group" />',
-			maxHeight: false,
-			buttonText: function(options) {
-				if (options.length == 0) {
-				    return '<span class="_pull-left">(all)</span> <span class="caret"></span>';
-				} else if (options.length > 1) {
-					return '<span class="_pull-left">' + options.length + ' selected</span> <span class="caret"></span>';
-				} else {
-					var selected = '';
-					options.each(function() {
-						selected += $(this).text() + ', ';
-					});
-					return '<span class="_pull-left">' + selected.substr(0, selected.length - 2) + '</span> <span class="caret"></span>';
-				}
-			}
-		});
+    directive.link = function(scope, element, attrs) {
+        scope.loading = true;
+        scope.selectedCategories = {};
 
-		CategoryDataProvider.getCategories(
-			function(data) {
-				scope.categories = data;
-				//console.log("success");
-				setTimeout(function() {
-					 element.find("select").multiselect('rebuild');
-				}, 0.5*1000); // little delay is needed
-			},
-			function(error) { throw Error("Fail to load categories. " + error); },
-			function() { scope.loading = false; }
-		);
+        scope.onChange = function (element, selected) {
+            alert(selected);
+            scope.selectedCategories = selected;
+        };
 
-	};
+        element.find("select").multiselect({
+            templates: {
+                button: '<button type="button" class="multiselect dropdown-toggle form-control" data-toggle="dropdown"></button>'
+            },
+            buttonClass: 'btn btn-default',
+            buttonWidth: 'auto',
+            buttonContainer: '<div class="btn-group" />',
+            maxHeight: false,
+            onChange: scope.onChange,
+            buttonText: function(options) {
+                if (options.length == 0) {
+                    return '<span>(all)</span> <span class="caret"></span>';
+                } else if (options.length > 1) {
+                    return '<span>' + options.length + ' selected</span> <span class="caret"></span>';
+                } else {
+                    var selected = '';
+                    options.each(function() {
+                        selected += $(this).text() + ', ';
+                    });
+                    return '<span>' + selected.substr(0, selected.length - 2) + '</span> <span class="caret"></span>';
+                }
+            }
+        });
 
-	return directive;
+        CategoryDataProvider.getCategories(
+            function(data) {
+                scope.categories = data;
+                setTimeout(function() {
+                     element.find("select").multiselect('rebuild');
+                }, 0.5*1000); // little delay is needed
+            },
+            function(error) { throw Error("Fail to load categories. " + error); },
+            function() { scope.loading = false; }
+        );
+        
+    };
+
+    return directive;
 }]);
 
