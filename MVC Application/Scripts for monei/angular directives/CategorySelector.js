d41d083c64dc6998ec42e506199e18130884ebf5
Alex75 <alessandro.piccione.75@gmail.com>
2016-07-17 11:58:18 +0100

    Add function to remove empty option ("unknown option") created by Angular when "multiple" attribute is not set on the "select" element. Call creation of multiple select after the loading of data.

--- MVC Application/Scripts for monei/angular directives/CategorySelector.js
+++ MVC Application/Scripts for monei/angular directives/CategorySelector.js
@@ -8,47 +8,62 @@ function (utils, CategoryDataProvider) {
     directive.templateUrl = "/Scripts for monei/directive-templates/CategorySelector.html";
 
     directive.scope = {
-        selectedCategories: "="
+        selectedCategories: "=" // the container should use the property "selected-categories"
     };
 
     directive.link = function(scope, element, attrs) {
         scope.loading = true;
+        
+        var select = element; // if a container is used use element.find("select")
+        //scope.select = select;
 
-        scope.onChange = function (element, selected) {
-            //alert(selected + " 123");
-            //scope.selectedCategories = selected;
+        function createMultiselect() {
+            alert("createMultiselect");
+            if (attrs.multiselect == 'true') {
+
+                element.attr("multiple", "multiple");
+
+                removeEmptyOption(select);
+
+                select.multiselect({
+                    templates: {
+                        button: '<button type="button" class="multiselect dropdown-toggle form-control" data-toggle="dropdown" style="min-width:100px"></button>'
+                    },
+                    buttonClass: 'btn btn-default',
+                    buttonWidth: 'auto',
+                    buttonContainer: '<div class="btn-group" />',
+                    maxHeight: false,
+                    //onChange:
+                    buttonText: function (selectedOptions) {
+                        if (selectedOptions.length == 0) {
+                            return '<span>(all)</span> <span class="caret"></span>';
+                        } else if (selectedOptions.length > 3) {
+                            return '<span>' + selectedOptions.length + ' selected</span> <span class="caret"></span>';
+                        } else {
+                            var selected = '';
+
+                            selectedOptions.each(function () {
+                                selected += $(this).text() + ', ';
+                            });
+                            return '<span>' + selected.substr(0, selected.length - 2) + '</span> <span class="caret"></span>';
+                        }
+                    }
+                });
+            }
         };
 
-        element.find("select").multiselect({
-            templates: {
-                button: '<button type="button" class="multiselect dropdown-toggle form-control" data-toggle="dropdown" style="min-width:100px"</button>'
-            },
-            buttonClass: 'btn btn-default',
-            buttonWidth: 'auto',
-            buttonContainer: '<div class="btn-group" />',
-            maxHeight: false,
-            onChange: scope.onChange,
-            buttonText: function(options) {
-                if (options.length == 0) {
-                    return '<span>(all)</span> <span class="caret"></span>';
-                } else if (options.length > 1) {
-                    return '<span>' + options.length + ' selected</span> <span class="caret"></span>';
-                } else {
-                    var selected = '';
-                    options.each(function() {
-                        selected += $(this).text() + ', ';
-                    });
-                    return '<span>' + selected.substr(0, selected.length - 2) + '</span> <span class="caret"></span>';
-                }
-            }
-        });
+        function removeEmptyOption(selectElement) {
+            // remove unknown empty option cretaed by Angular because "multiple" prop is not set at angular compile time.
+            $('option[value="?"]', selectElement).remove();
+        }
 
         CategoryDataProvider.getCategories(
             function(data) {
                 scope.categories = data;
-                setTimeout(function() {
-                     element.find("select").multiselect('rebuild');
-                }, 0.5*1000); // little delay is needed
+
+                setTimeout(function () {
+                    createMultiselect();
+                }, 0.5 * 1000); // a little delay is needed
             },
             function(error) { throw Error("Fail to load categories. " + error); },
             function() { scope.loading = false; }
