a1de7f8e2d01d9e3e9f8b0145ac13851967e90e5
Alex75 <alessandro.piccione.75@gmail.com>
2016-07-20 20:11:11 +0100

    Add a method in Category API to return the list ordered by most used to less one. NOT implemented.

--- MVC Application/Api/CategoryApiController.cs
+++ MVC Application/Api/CategoryApiController.cs
@@ -11,60 +11,64 @@ using Monei.MvcApplication.Controllers.Api.PostDataObjects;
 
 namespace Monei.MvcApplication.Api
 {	
-	[RoutePrefix("api/category")]
-	public class CategoryApiController : ApiControllerBase
-	{
-		[HttpGet, Route("ping")]
-		public void Ping()
-		{
-			//return "pong";
-		}
+    [RoutePrefix("api/category")]
+    public class CategoryApiController : ApiControllerBase
+    {
+        [HttpGet, Route("ping")]
+        public void Ping()
+        {
+            //return "pong";
+        }
 
-		[HttpGet, Route("list")]
-		public IEnumerable<Category> Get()
-		{
-			
-			//IList<Category> list = new List<Category>() { };
-			//list.Add(new Category() { 
-			//	Name = "AAA",
-			//});
+        [HttpGet, Route("list")]
+        public IEnumerable<Category> Get()
+        {
+            IEnumerable<Category> list = base.CategoryRepository.List();
+            foreach(var category in list)
+            {
+                category.Subcategories = null;
+            }
 
-			//System.Collections.Generic.IList<Subcategory>
+            return list;
+        }
 
-			IEnumerable<Category> list = base.CategoryRepository.List();
-			foreach(var category in list)
-			{
-				category.Subcategories = null;
-			}
+        [HttpGet, Route("list")]
+        public IEnumerable<Category> Get(string orderBy)
+        {
+            IEnumerable<Category> list = base.CategoryRepository.List();
+            foreach (var category in list)
+            {
+                category.Subcategories = null;
+            }
 
-			return list;
-		}
+            return list;
+        }
 
-		[HttpGet /*Route("action/Category/Tree", Name = "Tree")*/]
-		public IList<dynamic> Tree()
-		{
-			IList<dynamic> list = CategoryRepository.ListWithSubcategories().Select(c =>
-				new { id = c.Id, name = c.Name, subcategories = c.Subcategories.Select(s => new { id = s.Id, name = s.Name }).ToList() })
-				.ToList<dynamic>();
-			return list;
-		}
+        [HttpGet /*Route("action/Category/Tree", Name = "Tree")*/]
+        public IList<dynamic> Tree()
+        {
+            IList<dynamic> list = CategoryRepository.ListWithSubcategories().Select(c =>
+                new { id = c.Id, name = c.Name, subcategories = c.Subcategories.Select(s => new { id = s.Id, name = s.Name }).ToList() })
+                .ToList<dynamic>();
+            return list;
+        }
 
 
-		[HttpPost]
-		//public bool MoveSubcategory([FromBody]int subcategoryId, [FromBody]int categoryId)
-		public string MoveSubcategory(MoveSubcategoryPostData data)
-		{
-			try
-			{
-				CategoryRepository.MoveSubcategory(data.SubcategoryId, data.CategoryId);
-				return "OK";
-			}
-			catch (Exception exc)
-			{
-				Guid errorId = Guid.NewGuid();
-				logger.ErrorFormat("Fail to move Subcategory. Subcategory ID: {0}, Category ID: {1}. Error ID: {2}. {3}.", data.SubcategoryId, data.CategoryId, errorId, exc);
-				return string.Format("Fail to move Subcategory. Error ID: {0}.", errorId);
-			}
-		}
-	}
+        [HttpPost]
+        //public bool MoveSubcategory([FromBody]int subcategoryId, [FromBody]int categoryId)
+        public string MoveSubcategory(MoveSubcategoryPostData data)
+        {
+            try
+            {
+                CategoryRepository.MoveSubcategory(data.SubcategoryId, data.CategoryId);
+                return "OK";
+            }
+            catch (Exception exc)
+            {
+                Guid errorId = Guid.NewGuid();
+                logger.ErrorFormat("Fail to move Subcategory. Subcategory ID: {0}, Category ID: {1}. Error ID: {2}. {3}.", data.SubcategoryId, data.CategoryId, errorId, exc);
+                return string.Format("Fail to move Subcategory. Error ID: {0}.", errorId);
+            }
+        }
+    }
 }
--- Unit Test/MVC Application/Api/CategoryApiControllerTest.cs
+++ Unit Test/MVC Application/Api/CategoryApiControllerTest.cs
@@ -12,29 +12,53 @@ using Should;
 
 namespace Monei.Test.UnitTest.MvcApplication.Api
 {
-	[TestFixture, Category("Web")]
-	public class CategoryControllerTest
-	{
-		//todo: this test is no more necessary, use it as template and delete after first "use"
-		[Test]
-		public void Get()
-		{
-			// Arrange
-			var controller = new CategoryApiController();
+    [TestFixture, Category("Web")]
+    public class CategoryControllerTest
+    {
+        //todo: this test is no more necessary, use it as template and delete after first "use"
+        [Test]
+        public void Get()
+        {
+            // Arrange
+            var controller = new CategoryApiController();
 
-			ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
-			IList<Category> data = new List<Category>();
-			data.Add(new Category() { Id = 1, Name = "Test" });
-			A.CallTo(() => categoryRepository.List()).Returns(data);
-			controller.CategoryRepository = categoryRepository;
+            ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
+            IList<Category> data = new List<Category>();
+            data.Add(new Category() { Id = 1, Name = "Test" });
+            A.CallTo(() => categoryRepository.List()).Returns(data);
+            controller.CategoryRepository = categoryRepository;
 
-			// Act
-			IEnumerable<Category> list = controller.Get();
+            // Act
+            IEnumerable<Category> list = controller.Get();
 
-			// Assert
-			list.ShouldNotBeEmpty();
-		}
-		
+            // Assert
+            list.ShouldNotBeEmpty();
+        }
 
-	}
+        [Test]
+        public void List_when_OrderByMostUsed_then_ReturnedList_should_BeOrderedByUsage()
+        {
+            // Arrange
+            var controller = new CategoryApiController();
+
+            ICategoryRepository categoryRepository = A.Fake<ICategoryRepository>();
+            IList<Category> data = new List<Category>();
+            data.Add(new Category() { Id = 1, Name = "Test" });
+            A.CallTo(() => categoryRepository.List()).Returns(data);
+            controller.CategoryRepository = categoryRepository;
+
+            // Act
+            string orderBy = "mostUsed";
+            IEnumerable<Category> list = controller.Get(orderBy);
+
+            // Assert
+            list.ShouldNotBeEmpty();
+
+            var firstItem = list.First();
+            var lastItem = list.Last();
+
+            Assert.Inconclusive("because the returned items haven't a parameter for the number of use.");
+            //firstItem.?
+        }
+    }
 }
