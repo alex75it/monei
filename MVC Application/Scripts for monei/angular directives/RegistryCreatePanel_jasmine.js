6ab62bde29a07bd8acbfa059e9d2266f5848ee9b
Alex75 <alessandro.piccione.75@gmail.com>
2016-08-11 22:11:13 +0100

    Move Jasmine test runner in "Scripts for monei" folder. Rename folder "Jasmine Test" to "Jasmine". Delete old or unused files.

--- MVC Application/Scripts for monei/angular directives/RegistryCreatePanel_jasmine.js
+++ MVC Application/Scripts for monei/angular directives/RegistryCreatePanel_jasmine.js
@@ -5,8 +5,8 @@ describe("RegistryCreatePanel", function () {
     var appName = app.name; // this "initialize" module, all other way give an error
     beforeEach(angular.mock.module("monei")); // this is needed to make injection of providers works
 
-    http://stackoverflow.com/questions/14238490/injecting-dependent-services-when-unit-testing-angularjs-services
-    http://stackoverflow.com/questions/14773269/injecting-a-mock-into-an-angularjs-service
+    //http://stackoverflow.com/questions/14238490/injecting-dependent-services-when-unit-testing-angularjs-services
+    //http://stackoverflow.com/questions/14773269/injecting-a-mock-into-an-angularjs-service
 
     var panel;
 
@@ -21,7 +21,7 @@ describe("RegistryCreatePanel", function () {
     });
 
     describe("save()", function () {
-        xit("is defined", function () {
+        it("is defined", function () {
             expect(panel.save).toBeDefined();
         });
     });
