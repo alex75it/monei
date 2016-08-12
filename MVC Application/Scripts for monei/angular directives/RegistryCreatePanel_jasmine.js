ba66c88b71fbd9d285090c60e3d1c89de015526b
Alex75 <alessandro.piccione.75@gmail.com>
2016-08-12 22:07:23 +0100

    Add empty Jasmine test files to remove 404 errors noise.

--- MVC Application/Scripts for monei/angular directives/RegistryCreatePanel_jasmine.js
+++ MVC Application/Scripts for monei/angular directives/RegistryCreatePanel_jasmine.js
@@ -5,16 +5,45 @@ describe("RegistryCreatePanel", function () {
     var appName = app.name; // this "initialize" module, all other way give an error
     beforeEach(angular.mock.module("monei")); // this is needed to make injection of providers works
 
+    /**
+    beforeEach(function () {
+        var mockModule = angular.module("mockServices", [])
+        //["$timeout", "CategoryDataProvider", "RegistryDataProvider", "NotificationService", 
+        mockModule.service("CategoryDataProvider", function () {
+            
+        });
+
+        mockModule.service("RegistryDataProvider", function () {
+
+        });
+
+        mockModule.service("NotificationService", function () {
+
+        });
+        var app = angular.module("monei", [mockModule]);
+
+        alert("mocked");
+    });
+    */
+    
+
+    
+
     //http://stackoverflow.com/questions/14238490/injecting-dependent-services-when-unit-testing-angularjs-services
     //http://stackoverflow.com/questions/14773269/injecting-a-mock-into-an-angularjs-service
 
     var panel;
 
-    beforeEach(inject(function (_RegistryCreatePanel_) {
+    //beforeEach(inject(function (/*RegistryCreatePanel*/RegistryDataProvider) {
+    beforeEach(function () {
 
-        alert(_RegistryCreatePanel_);
-        panel = _RegistryCreatePanel_;
-    }));
+        
+
+        //alert(RegistryCreatePanel);
+        //panel = RegistryCreatePanel;
+
+        //alert("RegistryDataProvider: " + RegistryDataProvider);
+    });
 
     it("is injected", function () {
         expect(panel).toBeDefined();
