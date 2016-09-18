c4a47ea20ac001daadb477c889e637db75d6e073
Alex75 <alessandro.piccione.75@gmail.com>
2016-09-18 22:50:55 +0100

    Add validation of date, amount, operation and description fields.

--- MVC Application/Scripts for monei/angular providers/RegistryDataProvider.js
+++ MVC Application/Scripts for monei/angular providers/RegistryDataProvider.js
@@ -6,12 +6,24 @@ function ($http, utils) {
 
     var provider = {};
 
+    provider.OPERATION_TYPE_TRANSFER = "Transfer";
+        
     provider.search = function(filters, callback, errorCallback, finallyCallback) {
         $http.post(baseUrl + "/search", filters)
             .success(callback)
             .error(function(error, status) { errorCallback && errorCallback(error) })
             .finally(function() { finallyCallback && finallyCallback() });
     };
+
+    provider.validateData = function (data)
+    {
+        if (!data) throw Error("Data is null");
+        
+        if (!data.date) throw Error("Date must be specified");
+        if(!data.categoryId) throw new Error("Category must be specified");
+        if ((!data.amount || data.amount == 0) && data.operation != provider.OPERATION_TYPE_TRANSFER) throw Error("Amount must be specified");
+        if (!data.description || data.description.length == 0) throw Error("Description cannot be empty");
+    }
 
     provider.save = function (record, callback, errorCallback, finallyCallback) {
         $http.post(baseUrl, record)
