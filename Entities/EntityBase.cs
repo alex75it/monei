e5293a0dea2fd82d50f50bfc48beab27f340753d
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-02 23:43:29 +0100

    (cleanup)

--- Entities/EntityBase.cs
+++ Entities/EntityBase.cs
@@ -16,19 +16,7 @@ namespace Monei.Entities
 			CreationDate = DateTime.UtcNow;
 		}
 
-		//public static EntityBase New()
-		//{
-		//	return new EntityBase()
-		//	{
-		//		CreationDate = DateTime.Now,
-		//	};
-		//}
-
 		public virtual TKey Id { get; set; }
-		//[BsonId]
-		//public ObjectId Id {get; set;}
-
-		//[ScaffoldColumn(false)]
 		public virtual DateTime CreationDate { get; set; }
 		public virtual DateTime? LastChangeDate { get; set; }
 		public virtual Account CreationAccount { get; set; }
