02a04d88b3cd94ec0163f72de0d02167dca4619a
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-02 22:58:22 +0100

    (cleanup)

--- Unit Test/TestBase.cs
+++ Unit Test/TestBase.cs
@@ -7,17 +7,16 @@ using System.Threading.Tasks;
 
 namespace Monei.Test.UnitTest
 {
-	public class TestBase
-	{
-		protected DateTime minSqlDate = System.Data.SqlTypes.SqlDateTime.MinValue.Value;
-		protected DateTime maxSqlDate = System.Data.SqlTypes.SqlDateTime.MaxValue.Value;
+    public class TestBase
+    {
+        protected DateTime minSqlDate = System.Data.SqlTypes.SqlDateTime.MinValue.Value;
+        protected DateTime maxSqlDate = System.Data.SqlTypes.SqlDateTime.MaxValue.Value;
 
-		protected bool IsValidSqlDate(DateTime date)
-		{
-			return date >= minSqlDate
-			       && date <= maxSqlDate;
-		}
-
+        protected bool IsValidSqlDate(DateTime date)
+        {
+            return date >= minSqlDate
+                   && date <= maxSqlDate;
+        }
 
         protected void AssertExceptionIsRaised(Action action, Exception expectedException)
         {
