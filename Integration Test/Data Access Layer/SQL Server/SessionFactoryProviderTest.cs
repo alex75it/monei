a313f2de4379176be63b43263a9cb16d1582b792
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-16 10:13:02 +0100

    Add SessionFactoryProvider test.

---
+++ Integration Test/Data Access Layer/SQL Server/SessionFactoryProviderTest.cs
@@ -0,0 +1,31 @@
+﻿using Monei.DataAccessLayer.SqlServer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monei.Test.IntegrationTest.Data_Access_Layer.SQL_Server
+{
+    [TestFixture]
+    public class SessionFactoryProviderTest
+    {
+        [Test]
+        public void GetSessionFactory_when_CalledMultipleTimes_should_ReturnAValidSession()
+        {
+            SessionFactoryProvider provider = new SessionFactoryProvider();
+
+            var sessionFactory = provider.GetSessionFactory();
+            using (sessionFactory.OpenSession())
+            {
+                // ...
+            }
+
+            using (sessionFactory.OpenSession())
+            {
+                // ...
+            }
+        }
+    }
+}
